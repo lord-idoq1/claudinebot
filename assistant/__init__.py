e1de65ae50eb9e4019855448bd0a3039efc4e5c2
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-11-28 17:14:31 +0000

    pylint: auto fixes

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -7,13 +7,13 @@
 
 from telethon import Button, custom
 
-from plugins import ATRA_COL, InlinePlugin
 from Ayra import *
 from Ayra import _ayra_cache
 from Ayra._misc import owner_and_sudos
 from Ayra._misc._assistant import asst_cmd, callback, in_pattern
 from Ayra.fns.helper import *
 from Ayra.fns.tools import get_stored_file
+from plugins import ATRA_COL, InlinePlugin
 from strings import get_languages, get_string
 
 OWNER_NAME = ayra_bot.full_name
