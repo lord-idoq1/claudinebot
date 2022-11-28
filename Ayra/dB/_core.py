4c6e64b8d6eec5457468f3a4bffe0785a5143f75
senpai80 <73426077+senpai80@users.noreply.github.com>
2022-11-28 16:33:15 +0700

    Add files via upload

---
+++ Ayra/dB/_core.py
@@ -0,0 +1,13 @@
+# Ayra - UserBot
+# Copyright (C) 2021-2022 Teamayra
+#
+# This file is a part of < https://github.com/senpai80/Ayra/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/senpai80/Ayra/blob/main/LICENSE>.
+
+PLUGINS = []
+ADDONS = []
+HELP = {}
+LOADED = {}
+LIST = {}
+VC_HELP = {}
