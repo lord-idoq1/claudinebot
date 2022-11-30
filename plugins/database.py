5d8d5052de1c2a39940fe99aa9756680352e30df
senpai80 <73426077+senpai80@users.noreply.github.com>
2022-12-01 03:18:37 +0700

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -16,11 +16,11 @@ import re
 from . import Redis, ayra_cmd, eor, get_string, udB
 
 
-@ayra_cmd(pattern="setdb( (.*)|$)", fullsudo=True)
-async def _(ayra):
-    match = ayra.pattern_match.group(1).strip()
+@ayra_cmd(pattern="setdb( (.*)|$)", fullsudo=False)
+async def _(ay):
+    match = ay.pattern_match.group(1).strip()
     if not match:
-        return await ayra.eor("Berikan kunci dan nilai untuk ditetapkan!")
+        return await ay.eor("Berikan kunci dan nilai untuk ditetapkan!")
     try:
         delim = " " if re.search("[|]", match) is None else " | "
         data = match.split(delim, maxsplit=1)
@@ -28,19 +28,19 @@ async def _(ayra):
             data = data[1].split(maxsplit=1)
             data[1] = f"{str(udB.get_key(data[0]))} {data[1]}"
         udB.set_key(data[0], data[1])
-        await ayra.eor(
+        await ay.eor(
             f"**Pasangan Nilai Kunci DB Diperbarui\nKunci :** `{data[0]}`\n**Value :** `{data[1]}`"
         )
 
     except BaseException:
-        await ayra.eor(get_string("com_7"))
+        await ay.eor(get_string("com_7"))
 
 
-@ayra_cmd(pattern="deldb( (.*)|$)", fullsudo=True)
-async def _(ayra):
-    key = ayra.pattern_match.group(1).strip()
+@ayra_cmd(pattern="deldb( (.*)|$)", fullsudo=False)
+async def _(ay):
+    key = ay.pattern_match.group(1).strip()
     if not key:
-        return await ayra.eor("Beri saya nama kunci untuk dihapus!", time=5)
+        return await ay.eor("Beri saya nama kunci untuk dihapus!", time=5)
     _ = key.split(maxsplit=1)
     try:
         if _[0] == "-m":
@@ -50,28 +50,28 @@ async def _(ayra):
         else:
             k = udB.del_key(key)
         if k == 0:
-            return await ayra.eor("`Tidak Ada Kunci Seperti Itu.`")
-        await ayra.eor(f"`Kunci berhasil dihapus {key}`")
+            return await ay.eor("`Tidak Ada Kunci Seperti Itu.`")
+        await ay.eor(f"`Kunci berhasil dihapus {key}`")
     except BaseException:
-        await ayra.eor(get_string("com_7"))
+        await ay.eor(get_string("com_7"))
 
 
-@ayra_cmd(pattern="rendb( (.*)|$)", fullsudo=True)
-async def _(ayra):
-    match = ayra.pattern_match.group(1).strip()
+@ayra_cmd(pattern="rendb( (.*)|$)", fullsudo=False)
+async def _(ay):
+    match = ay.pattern_match.group(1).strip()
     if not match:
-        return await ayra.eor("`Berikan nama Kunci untuk mengganti nama..`")
+        return await ay.eor("`Berikan nama Kunci untuk mengganti nama..`")
     delim = " " if re.search("[|]", match) is None else " | "
     data = match.split(delim)
     if Redis(data[0]):
         try:
             udB.rename(data[0], data[1])
             await eor(
-                ult,
+                ay,
                 f"**Penggantian Nama Kunci DB Berhasil\nKunci Lama :** `{data[0]}`\n**New Key :** `{data[1]}`",
             )
 
         except BaseException:
-            await ayra.eor(get_string("com_7"))
+            await ay.eor(get_string("com_7"))
     else:
-        await ayra.eor("Kunci tidak ditemukan")
+        await ay.eor("Kunci tidak ditemukan")
