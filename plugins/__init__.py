d178e5a584d0b4f77606a2b08244231bfba6474f
senpai80 <73426077+senpai80@users.noreply.github.com>
2022-12-02 16:08:48 +0700

    Add files via upload

---
+++ plugins/__init__.py
@@ -0,0 +1,92 @@
+# Ayra - UserBot
+# Copyright (C) 2021-2022 senpai80
+#
+# This file is a part of < https://github.com/senpai80/Ayra/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
+
+import asyncio
+import os
+import time
+from random import choice
+
+import requests
+from telethon import Button, events
+from telethon.tl import functions, types  # pylint:ignore
+
+from Ayra import *
+from Ayra._misc._assistant import asst_cmd, callback, in_pattern
+from Ayra._misc._decorators import ayra_cmd
+from Ayra._misc._wrappers import eod, eor
+from Ayra.dB import DEVLIST, AYRA_IMAGES
+from Ayra.fns.helper import *
+from Ayra.fns.info import *
+from Ayra.fns.misc import *
+from Ayra.fns.tools import *
+from Ayra.version import __version__, ayra_version
+from strings import get_help, get_string
+
+Redis = udB.get_key
+con = TgConverter
+quotly = Quotly()
+OWNER_NAME = get_string("devs")
+OWNER_ID = ayra_bot.uid
+
+LOG_CHANNEL = udB.get_key("LOG_CHANNEL")
+
+
+def inline_pic():
+    INLINE_PIC = udB.get_key("INLINE_PIC")
+    if INLINE_PIC is None:
+        INLINE_PIC = choice(AYRA_IMAGES)
+    elif INLINE_PIC == False:
+        INLINE_PIC = None
+    return INLINE_PIC
+
+
+Telegraph = telegraph_client()
+
+List = []
+Dict = {}
+InlinePlugin = {}
+N = 0
+cmd = ayra_cmd
+STUFF = {}
+
+# Chats, which needs to be ignore for some cases
+# Considerably, there can be many
+# Feel Free to Add Any other...
+
+NOSPAM_CHAT = [
+    -1001361294038,  # UltroidSupportChat
+    -1001387666944,  # PyrogramChat
+    -1001109500936,  # TelethonChat
+    -1001050982793,  # Python
+    -1001256902287,  # DurovsChat
+    -1001473548283,  # SharingUserbot
+    -1001601365018,  # Stuf Chat
+    --1001687155877,  # Cilik Support
+]
+
+KANGING_STR = [
+    "Plagiat hehe...",
+    "Ijin bre comot stiker ini ke paket saya...",
+    "Boleh juga nih...",
+    "Keren juga nih...",
+    "Lah apaan tuh...",
+]
+
+ATRA_COL = [
+    "DarkCyan",
+    "DeepSkyBlue",
+    "DarkTurquoise",
+    "Cyan",
+    "LightSkyBlue",
+    "Turquoise",
+    "MediumVioletRed",
+    "Aquamarine",
+    "Lightcyan",
+    "Azure",
+    "Moccasin",
+    "PowderBlue",
+]
