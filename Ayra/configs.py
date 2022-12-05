7b1112329b2ae7e48a01ba0146d518445b709471
senpai80 <73426077+senpai80@users.noreply.github.com>
2022-12-05 12:59:14 +0700

    Update configs.py

--- Ayra/configs.py
+++ Ayra/configs.py
@@ -76,5 +76,5 @@ class Var:
     or message.outgoing
 )
 
-bot = Client(STRING_SESSION, API_ID, API_HASH, plugins=dict(root="main"))
+bot = Client(STRING_SESSION, API_ID, API_HASH, plugins=dict(root="plugins"))
 call_py = PyTgCalls(bot)
