a6fb40698ba114ed5ffd201816f18b344042c4d5
senpai80 <73426077+senpai80@users.noreply.github.com>
2022-12-05 13:22:11 +0700

    Update decorators.py

--- Ayra/helpers/decorators.py
+++ Ayra/helpers/decorators.py
@@ -3,7 +3,7 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 
-from Ayra.configs import SUDO_USERS
+from config import SUDO_USERS
 from Ayra.helpers.admins import get_administrators
 
 
