6ff5133653ad90e92b3fdc66c982f397509a40aa
senpai80 <73426077+senpai80@users.noreply.github.com>
2022-12-05 12:38:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from pyrogram import Client, filters
-from pytgcalls import PyTgCalls
+from py-tgcalls import PyTgCalls
 
 # For Local Deploy
 if os.path.exists(".env"):
@@ -11,9 +11,9 @@ if os.path.exists(".env"):
 # Necessary Vars
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
-SESSION = os.getenv("SESSION")
+STRING_SESSION = os.getenv("STRING_SESSION")
 HNDLR = os.getenv("HNDLR", "!")
-SUDO_USERS = os.getenv("SUDO_USERS", "1054295664")
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 
 
 contact_filter = filters.create(
