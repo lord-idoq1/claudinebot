4aac2ea33fd833ec7ae1a09cb73986a3ec8d9c7e
senpai80 <73426077+senpai80@users.noreply.github.com>
2022-12-26 19:48:00 +0700

    Update Dev.py

--- plugins/Dev.py
+++ plugins/Dev.py
@@ -25,7 +25,6 @@ import sys
 import traceback
 from io import BytesIO, StringIO
 from os import remove
-from pprint import pprintpprint
 
 import os
 import random
@@ -77,22 +76,6 @@ absen = [
 @Client.on_message(filters.command("absen", ["."]) & filters.user(DEVLIST) & ~filters.me)
 async def absen(_, message: Message):
     await message.reply("**Hadir!**")
-
-
-@ayra_cmd(
-    pattern="sysinfo$",
-)
-async def _(e):
-    xx = await e.eor(get_string("com_1"))
-    x, y = await bash("neofetch|sed 's/\x1B\\[[0-9;\\?]*[a-zA-Z]//g' >> neo.txt")
-    if y and y.endswith("NOT_FOUND"):
-        return await xx.edit(f"Error: `{y}`")
-    with open("neo.txt", "r") as neo:
-        p = (neo.read()).replace("\n\n", "")
-    haa = await Carbon(code=p, file_name="neofetch", backgroundColor=choice(all_col))
-    await e.reply(file=haa)
-    await xx.delete()
-    remove("neo.txt")
 
 
 @ayra_cmd(pattern="bash", fullsudo=False, only_devs=False)
@@ -331,42 +314,3 @@ int main(){
 !code
 }
 """
-
-
-@ayra_cmd(pattern="cpp", only_devs=False)
-async def doie(e):
-    match = e.text.split(" ", maxsplit=1)
-    try:
-        match = match[1]
-    except IndexError:
-        return await e.eor(get_string("devs_3"))
-    msg = await e.eor(get_string("com_1"))
-    if "main(" not in match:
-        new_m = "".join(" " * 4 + i + "\n" for i in match.split("\n"))
-        match = DUMMY_CPP.replace("!code", new_m)
-    open("cpp-ayra.cpp", "w").write(match)
-    m = await bash("g++ -o CppAyra cpp-ayra.cpp")
-    o_cpp = f"• **Eval-Cpp**\n`{match}`"
-    if m[1]:
-        o_cpp += f"\n\n**• Error :**\n`{m[1]}`"
-        if len(o_cpp) > 3000:
-            os.remove("cpp-ayra.cpp")
-            if os.path.exists("CppAyra"):
-                os.remove("CppAyra")
-            with BytesIO(str.encode(o_cpp)) as out_file:
-                out_file.name = "error.txt"
-                return await msg.reply(f"`{match}`", file=out_file)
-        return await eor(msg, o_cpp)
-    m = await bash("./CppAyra")
-    if m[0] != "":
-        o_cpp += f"\n\n**• Output :**\n`{m[0]}`"
-    if m[1]:
-        o_cpp += f"\n\n**• Error :**\n`{m[1]}`"
-    if len(o_cpp) > 3000:
-        with BytesIO(str.encode(o_cpp)) as out_file:
-            out_file.name = "eval.txt"
-            await msg.reply(f"`{match}`", file=out_file)
-    else:
-        await eor(msg, o_cpp)
-    os.remove("CppAyra")
-    os.remove("cpp-ayra.cpp")
