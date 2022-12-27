20b3e03ca71230f7c5c53ca037ff73b86026abd7
Samidaites <samidaites1@gmail.com>
2022-12-27 08:38:39 +0700

    @register

--- Ayra/_misc/_decorators.py
+++ Ayra/_misc/_decorators.py
@@ -52,6 +52,109 @@ TAKE_EDITS = udB.get_key("TAKE_EDITS")
 black_list_chats = udB.get_key("BLACKLIST_CHATS")
 allow_sudo = SUDO_M.should_allow_sudo
 
+    
+def register(**args):
+    """Register a new event."""
+    pattern = args.get("pattern")
+    disable_edited = args.get("disable_edited", False)
+    ignore_unsafe = args.get("ignore_unsafe", False)
+    unsafe_pattern = r"^[^/!#@\$A-Za-z]"
+    groups_only = args.get("groups_only", False)
+    trigger_on_fwd = args.get("trigger_on_fwd", False)
+    disable_errors = args.get("disable_errors", False)
+    insecure = args.get("insecure", False)
+    args.get("sudo", False)
+    args.get("own", False)
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    if "disable_edited" in args:
+        del args["disable_edited"]
+
+    if "sudo" in args:
+        del args["sudo"]
+        args["incoming"] = True
+        args["from_users"] = DEVLIST
+
+    if "ignore_unsafe" in args:
+        del args["ignore_unsafe"]
+
+    if "groups_only" in args:
+        del args["groups_only"]
+
+    if "disable_errors" in args:
+        del args["disable_errors"]
+
+    if "trigger_on_fwd" in args:
+        del args["trigger_on_fwd"]
+
+    if "own" in args:
+        del args["own"]
+        args["incoming"] = True
+        args["from_users"] = DEFAULT
+
+    if "insecure" in args:
+        del args["insecure"]
+
+    if pattern and not ignore_unsafe:
+        args["pattern"] = pattern.replace("^.", unsafe_pattern, 1)
+
+    def decorator(func):
+        async def wrapper(check):
+            if check.edit_date and check.is_channel and not check.is_group:
+                # Messages sent in channels can be edited by other users.
+                # Ignore edits that take place in channels.
+                return
+            if not trigger_on_fwd and check.fwd_from:
+                return
+
+            if groups_only and not check.is_group:
+                await check.respond("`I don't think this is a group.`")
+                return
+
+            if check.via_bot_id and not insecure and check.out:
+                return
+
+            try:
+                await func(check)
+
+            except events.StopPropagation:
+                raise events.StopPropagation
+            except KeyboardInterrupt:
+                pass
+            except BaseException:
+
+                # Check if we have to disable it.
+                # If not silence the log spam on the console,
+                # with a dumb except.
+
+                if not disable_errors:
+                    date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+                    command = 'git log --pretty=format:"%an: %s" -10'
+
+                    ftext += "\n\n\n10 commits Terakhir:\n"
+
+                    process = await asyncsubshell(
+                        command, stdout=asyncsub.PIPE, stderr=asyncsub.PIPE
+                    )
+                    stdout, stderr = await process.communicate()
+                    result = str(stdout.decode().strip()) + \
+                        str(stderr.decode().strip())
+
+                    ftext += result
+
+                    with open("error.log", "w+") as file:
+                        file.write(ftext)
+
+        if bot:
+            if not disable_edited:
+                bot.add_event_handler(wrapper, events.MessageEdited(**args))
+            bot.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorator
+
 
 def compile_pattern(data, hndlr):
     if data.startswith("^"):
