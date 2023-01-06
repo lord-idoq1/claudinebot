d6c8606a4dc290b44cc204bf23d1df5e29ed0389
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-06 12:13:14 +0700

    Update Dev.py

--- plugins/Dev.py
+++ plugins/Dev.py
@@ -28,8 +28,6 @@ from os import remove
 from pprint import pprint
 
 from telethon.utils import get_display_name
-from Ayra.fns.tools import Carbon
-from . import ATRA_COL
 from Ayra import _ignore_eval
 
 from . import *
@@ -98,7 +96,7 @@ async def _(event):
                 code=stdout,
                 file_name="bash",
                 download=True,
-                backgroundColor=choice(ATRA_COL),
+                backgroundColor=choice(all_col),
             )
             url = f"https://graph.org{uf(li)[-1]}"
             OUT = f"[\xad]({url}){OUT}"
